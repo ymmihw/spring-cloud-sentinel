c0e28b25930c85995aac60cfd1c7d788e0857ade
wanghaoming <wanghaoming@ctfo.com>
2021-04-12 13:41:43 +0800

    add lombok

--- src/main/java/com/ymmihw/spring/cloud/sentinel/controller/GreetingController.java
+++ src/main/java/com/ymmihw/spring/cloud/sentinel/controller/GreetingController.java
@@ -1,15 +1,15 @@
 package com.ymmihw.spring.cloud.sentinel.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.ymmihw.spring.cloud.sentinel.service.GreetingService;
+import lombok.RequiredArgsConstructor;
 
 @RestController
+@RequiredArgsConstructor
 public class GreetingController {
 
-  @Autowired
-  private GreetingService greetingService;
+  private final GreetingService greetingService;
 
   @GetMapping("/hello")
   public String getGreeting() {
